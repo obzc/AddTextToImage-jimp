f8b24f5b78297da442cd6ae61a9529f7c69e9465
BZC <oktaybzc@gmail.com>
2020-08-31 16:54:20 +0300

    format

--- index.js
+++ index.js
@@ -10,35 +10,33 @@ const H = 1200;
 const main = async () => {
   Jimp.read(IMAGE)
     .then((image) => {
-      
-    image.cover(W, H).quality(60);
+      image.cover(W, H).quality(60);
+      //image.color([{apply:'mix', params: ['black', 60]}])
+      image.color([{ apply: "darken", params: [30] }]);
 
-    //image.color([{apply:'mix', params: ['black', 60]}])
-    image.color([{ apply: "darken", params: [30] }]);
+      Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(function (font) {
+        image.print(
+          font,
+          0,
+          400,
+          {
+            text:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+          },
+          W,
+          H
+        );
 
-    Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(function (font) {
-      image.print(
-        font,
-        0,
-        400,
-        {
-          text:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-          alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-        },
-        W,
-        H
-      );
+        image.getBuffer(Jimp.MIME_JPEG, (err, buffer) => {
+          image.write(DIR + FILENAME);
+          console.log("image uploaded: " + DIR + FILENAME);
+        });
 
-      image.getBuffer(Jimp.MIME_JPEG, (err, buffer) => {
-        image.write(DIR + FILENAME);
-        console.log("image uploaded: " + DIR + FILENAME);
-      });
-
-      /*image.getBase64(Jimp.AUTO, function(err, data) {  // Add err
+        /*image.getBase64(Jimp.AUTO, function(err, data) {  // Add err
         console.log(data); 
       });*/
-    });
+      });
     })
     .catch((err) => {
       // Handle an exception.
