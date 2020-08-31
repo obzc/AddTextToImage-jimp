2c9ab9c647eb5556b8fcdae51d880753c56c015f
BZC <oktaybzc@gmail.com>
2020-08-31 16:29:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
 const Jimp = require("jimp");
 
-const ORIGINAL_IMAGE =
-  "https://i.pinimg.com/564x/e9/1d/07/e91d07b1b027e66ba878c9e5d02a0f6d.jpg";
-
-const LOGO = "https://seeklogo.com/images/I/istanbul-gedik-universitesi-logo-19DE1BA437-seeklogo.com.png";
-
-const LOGO_MARGIN_PERCENTAGE = 5;
-
-const FILENAME = "test.jpg";
+const FILENAME = "test2.jpg";
 
 const main = async () => {
-  const [image, logo] = await Promise.all([
-    Jimp.read(ORIGINAL_IMAGE),
+  Jimp.read(
+    "https://i.pinimg.com/564x/9e/05/f3/9e05f3df5d9835ee0aafeb04e1857c0a.jpg"
+  ).then(function (image) {
+    image.cover(800, 1200);
     
-    Jimp.read(LOGO),
-
-  ]);
-
+    //image.color([{apply:'mix', params: ['black', 60]}]) 
+    image.color([{ apply: 'darken', params: [30] }]);
 
 
-  logo.resize(100, Jimp.AUTO);
 
-  const xMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 500;
-  const yMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 500;
+    image.write("test2.jpg");
 
-  const X = image.bitmap.width - logo.bitmap.width - xMargin;
-  const Y = image.bitmap.height - logo.bitmap.height - yMargin;
+    Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(function (font) {
+        image.print(font, 0, 400, {
+            text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER
+        }, 800, 1200);
+       
 
-  return image.composite(logo, X, Y, [
-    {
-      mode: Jimp.BLEND_SCREEN,
-      opacitySource: 0.1,
-      opacityDest: 1
-    }
-  ]);
+
+      image.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+        image.write("test2.jpg");
+      });
+    });
+  });
 };
 
-main().then(image => image.write(FILENAME));
+main().then((image) => image.write(FILENAME));
